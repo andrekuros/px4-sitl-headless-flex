abfd9b979886c9adad47c5fc829948740486e2bf
Andre Kuroswiski <andrekuros@gmail.com>
2021-06-09 12:19:37 -0300

    Update entrypoint.sh
    New possibilities for customization

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 function show_help {
     echo ""
-    echo "Usage: ${0} [-h | -v VEHICLE | -w WORLD] [IP_API | IP_QGC IP_API]"
+    echo "Usage: ${0} [-h | -v VEHICLE | -w WORLD | -i SYSID | -a IP_API | -q IP_QGC ]"
     echo ""
     echo "Run a headless px4-gazebo simulation in a docker container. The"
     echo "available vehicles and worlds are the ones available in PX4"
@@ -11,9 +11,9 @@ function show_help {
     echo "  -h    Show this help"
     echo "  -v    Set the vehicle (default: iris)"
     echo "  -w    Set the world (default: empty)"
-    echo ""
-    echo "  <IP_API> is the IP to which PX4 will send MAVLink on UDP port 14540"
-    echo "  <IP_QGC> is the IP to which PX4 will send MAVLink on UDP port 14550"
+    echo "  -i    Set MAVLINK SYS_ID (default 1)"
+    echo "  -a    set the IP to which PX4 will send MAVLink on UDP port 14540"
+    echo "  -q    set the IP to which PX4 will send MAVLink on UDP port 14550"
     echo ""
     echo "By default, MAVLink is sent to the host."
 }
@@ -22,46 +22,25 @@ OPTIND=1 # Reset in case getopts has been used previously in the shell.
 
 vehicle=iris
 world=empty
+SYS_ID=1
 
-while getopts "h?v:w:" opt; do
+while getopts "h?v:w::i:a:q:" opt; 
+do
     case "$opt" in
-    h|\?)
-        show_help
-        exit 0
-        ;;
-    v)  vehicle=$OPTARG
-        ;;
-    w)  world=$OPTARG
-        ;;
+       h|\?)
+          show_help
+          exit 0;;
+       v)  vehicle=$OPTARG;;
+       w)  world=$OPTARG ;;
+       i)  SYS_ID=$OPTARG;;
+       a)  IP_API=$OPTARG;;
+       q)  IP_QGC=$OPTARG;;
     esac
 done
-
-shift $((OPTIND-1))
-
-# All the leftover arguments are supposed to be IPs
-for arg in "$@"
-do
-    if ! [[ ${arg} =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        echo "Error: invalid IP: ${arg}!"
-        echo ""
-        show_help
-        exit 1
-    fi
-done
-
-if [ "$#" -eq 1 ]; then
-    IP_QGC="$1"
-elif [ "$#" -eq 2 ]; then
-    IP_API="$1"
-    IP_QGC="$2"
-elif [ "$#" -gt 2 ]; then
-    show_help
-    exit 1;
-fi
 
 Xvfb :99 -screen 0 1600x1200x24+32 &
 ${SITL_RTSP_PROXY}/build/sitl_rtsp_proxy &
 
-source ${WORKSPACE_DIR}/edit_rcS.bash ${IP_API} ${IP_QGC} &&
+source ${WORKSPACE_DIR}/edit_rcS.bash ${SYS_ID} ${IP_QGC} ${IP_API} &&
 cd ${FIRMWARE_DIR} &&
 HEADLESS=1 make px4_sitl gazebo_${vehicle}__${world}
