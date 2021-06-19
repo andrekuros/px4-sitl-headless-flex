b5a4a47aa0e40c42f712c10c8b94f3611eb7eb5a
Andre Kuroswiski <andrekuros@gmail.com>
2021-06-18 22:37:31 -0300

    Add option to change Multiple MAV params

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,9 @@ function show_help {
     echo "  -v    Set the vehicle (default: iris)"
     echo "  -w    Set the world (default: empty)"
     echo "  -i    Set MAVLINK SYS_ID (default 1)"
-    echo "  -a    set the IP to which PX4 will send MAVLink on UDP port 14540"
-    echo "  -q    set the IP to which PX4 will send MAVLink on UDP port 14550"
+    echo "  -a    set the IP to which PX4 will send MAVLink on UDP port APIS (default 14540)"
+    echo "  -q    set the IP to which PX4 will send MAVLink on UDP port GCs (defaults 14550)"
+    echo "  -p    set any MAVLink param (multiples) (eg. -p MPC_XY_CRUISE 20 -p MIS_DIST_1WP 3000)"
     echo ""
     echo "By default, MAVLink is sent to the host."
 }
@@ -24,7 +25,7 @@ vehicle=iris
 world=empty
 SYS_ID=1
 
-while getopts "h?v:w::i:a:q:" opt; 
+while getopts "h?v:w::i:a:q:p" opt; 
 do
     case "$opt" in
        h|\?)
@@ -35,6 +36,7 @@ do
        i)  SYS_ID=$OPTARG;;
        a)  IP_API=$OPTARG;;
        q)  IP_QGC=$OPTARG;;
+       p)  PARAMS_SET+=("$OPTARG");;
     esac
 done
 
@@ -42,5 +44,12 @@ Xvfb :99 -screen 0 1600x1200x24+32 &
 ${SITL_RTSP_PROXY}/build/sitl_rtsp_proxy &
 
 source ${WORKSPACE_DIR}/edit_rcS.bash ${SYS_ID} ${IP_QGC} ${IP_API} &&
+
+#Add extra PX4 Params FROM "-P"
+CONFIG_PARAMS=${FIRMWARE_DIR}/build/px4_sitl_default/etc/init.d-posix/px4-rc.params
+for argument in "${PARAMS_SET[@]}"; do
+  echo "param set ${argument}" >> ${CONFIG_PARAMS}
+done
+
 cd ${FIRMWARE_DIR} &&
 HEADLESS=1 make px4_sitl gazebo_${vehicle}__${world}
