60bb45da13b5e50a5ca508c99b5ce78b3952ce67
Andre Kuroswiski <andrekuros@gmail.com>
2021-06-18 23:46:28 -0300

    Adjust IP/PORT config

--- edit_rcS.bash
+++ edit_rcS.bash
@@ -24,26 +24,16 @@ function get_vm_host_ip {
 }
 
 
-if [ "$#" -eq 2 ]; then
-    if ! is_ip_valid $2; then exit 1; fi
-    echo "14540 will be associated to $2"
-    API_PARAM="-t $2"
-elif [ "$#" -eq 3 ]; then
-    if ! is_ip_valid $2 || ! is_ip_valid $3; then exit 1; fi
-    echo "14550 will be associated to $2"
-    echo "14540 will be associated to $3"
-    QGC_PARAM="-t $2"
-    API_PARAM="-t $3"
-elif [ "$#" -gt 3 ]; then
-    echo "Invalid parameters: [<SYS_ID> <IP for 14550> <IP for 14540>]"
-    exit 1;
-fi
-
+if ! is_ip_valid $2 || ! is_ip_valid $3; then exit 1; fi
+echo "GCS Connection-> $2:$4"
+echo "API Connection-> $3:$5"
+GCS_PARAM="-t $2"
+API_PARAM="-t $3"
 
 # Broadcast doesn't work with docker from a VM (macOS or Windows), so we default to the vm host (host.docker.internal)
 if is_docker_vm; then
     VM_HOST=$(get_vm_host_ip)
-    QGC_PARAM=${QGC_PARAM:-"-t ${VM_HOST}"}
+    GCS_PARAM=${QGC_PARAM:-"-t ${VM_HOST}"}
     API_PARAM=${API_PARAM:-"-t ${VM_HOST}"}
 fi
 
@@ -51,8 +41,8 @@ CONFIG_FILE=${FIRMWARE_DIR}/build/px4_sitl_default/etc/init.d-posix/rcS
 CONFIG_MAV=${FIRMWARE_DIR}/build/px4_sitl_default/etc/init.d-posix/px4-rc.mavlink
 CONFIG_PARAMS=${FIRMWARE_DIR}/build/px4_sitl_default/etc/init.d-posix/px4-rc.params
 
-sed -i "s/mavlink start \-x \-u \$udp_gcs_port_local -r 4000000 -f/mavlink start -x -u \$udp_gcs_port_local -r 4000000 -f ${QGC_PARAM}/" ${CONFIG_MAV}
-sed -i "s/mavlink start -x -u \$udp_offboard_port_local -r 4000000 -f -m onboard -o \$udp_offboard_port_remote/mavlink start -x -u \$udp_offboard_port_local -r 4000000 -f -m onboard -o \$udp_offboard_port_remote ${API_PARAM}/" ${CONFIG_MAV}
+sed -i "s/mavlink start \-x \-u \$udp_gcs_port_local -r 4000000 -f/mavlink start -x -u ${4} -r 4000000 -f ${GCS_PARAM}/" ${CONFIG_MAV}
+sed -i "s/mavlink start -x -u \$udp_offboard_port_local -r 4000000 -f -m onboard -o \$udp_offboard_port_remote/mavlink start -x -u $5 -r 4000000 -f -m onboard -o \$udp_offboard_port_remote ${API_PARAM}/" ${CONFIG_MAV}
 sed -i "s/param set MAV_SYS_ID \$((px4_instance+1))/param set MAV_SYS_ID ${1}/" ${CONFIG_FILE}
 
 echo 'param set MAV_PROTO_VER 2' >> ${CONFIG_PARAMS}
